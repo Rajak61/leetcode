6fe79d32b5c25ccca19ad4795344b853152001fc
Rajak61 <90247744+Rajak61@users.noreply.github.com>
2024-02-28 11:26:59 +0530

    Time: 1 ms (76.37%) | Memory: 45.7 MB (18.17%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,39 +1,36 @@
 class Solution {
-      
-    //https://www.youtube.com/watch?v=dSxt3ZCbIqA&ab_channel=NikhilLohia
-    //Time 0(mn) Space 0(1)
-    public void setZeroes(int[][] matrix) {
-     boolean firstRow = false, firstCol = false;
- // Set markers in first row and first column
-for(int i = 0; i < matrix.length; i++){
-    for(int j = 0; j < matrix[0].length; j++) {
-        if(matrix[i][j] == 0) {
+  // https://www.youtube.com/watch?v=dSxt3ZCbIqA&ab_channel=NikhilLohia
+  // Time 0(mn) Space 0(1)
 
-if(i==0) {
-    firstRow = true;
-}
- if(j== 0){
-     firstCol = true;
- }
-matrix[0][j] = 0;
-matrix[i][0] = 0;
-}
-}
-}
-//Replace inner matrix
-for(int i = 1; i < matrix.length; i++){
-for (int j= 1; j < matrix[0].length; j++) {
-    if (matrix[i] [0] == 0 || matrix[0][j]== 0) {
-        matrix[i][j] = 0;
+  public void setZeroes(int[][] matrix) {
+    boolean firstRow = false, firstCol = false;
+    // Set markers in first row and first column
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        if (matrix[i][j] == 0) {
+          if (i == 0) {
+            firstRow = true;
+          }
+          if (j == 0) {
+            firstCol = true;
+          }
+          matrix[0][j] = 0;
+          matrix[i][0] = 0;
+        }
+      }
     }
-}
-}
-//Last remaining checks
-if(firstRow)
-for (int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
-if(firstCol)
-for(int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
-    
-}
-
+    // Replace inner matrix
+    for (int i = 1; i < matrix.length; i++) {
+      for (int j = 1; j < matrix[0].length; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+    // Last remaining checks
+    if (firstRow)
+      for (int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
+    if (firstCol)
+      for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+  }
 }
