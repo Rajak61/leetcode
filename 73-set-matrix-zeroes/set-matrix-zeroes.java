d8c1dc75f69732433c4ca0a898faa99c4d8c57a9
Rajak61 <90247744+Rajak61@users.noreply.github.com>
2024-02-28 11:17:50 +0530

    Time: 1 ms (76.37%) | Memory: 45.8 MB (18.17%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,9 +1,10 @@
 class Solution {
+      
+    //https://www.youtube.com/watch?v=dSxt3ZCbIqA&ab_channel=NikhilLohia
+    //Time 0(mn) Space 0(1)
     public void setZeroes(int[][] matrix) {
-     
-        
-boolean firstRow = false, firstCol = false;
-// Set markers in first row and first column
+     boolean firstRow = false, firstCol = false;
+ // Set markers in first row and first column
 for(int i = 0; i < matrix.length; i++){
     for(int j = 0; j < matrix[0].length; j++) {
         if(matrix[i][j] == 0) {
@@ -17,14 +18,16 @@ if(i==0) {
 matrix[0][j] = 0;
 matrix[i][0] = 0;
 }
-    }}
+}
+}
 //Replace inner matrix
 for(int i = 1; i < matrix.length; i++){
-
 for (int j= 1; j < matrix[0].length; j++) {
     if (matrix[i] [0] == 0 || matrix[0][j]== 0) {
         matrix[i][j] = 0;
-    }}}
+    }
+}
+}
 //Last remaining checks
 if(firstRow)
 for (int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
