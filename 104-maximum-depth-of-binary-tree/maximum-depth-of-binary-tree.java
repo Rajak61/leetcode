7271e31c2d11c9d024d86dbbaf927cd6c9f932a9
Rajak61 <90247744+Rajak61@users.noreply.github.com>
2024-07-11 10:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (18.59%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,9 +14,9 @@
  * }
  */
 ///Time:0(n),space 0(1)
-	//Theory https://www.youtube.com/watch?v=hTM3phVI6YQ&ab_channel=NeetCode
+	//Theory https://www.youtube.com/watch?v=hTM3phVI6YQ&ab_channel=NeetCode ---DFS recusrsion
 	//Code -https://walkccc.me/LeetCode/problems/104/#__tabbed_1_2
-	
+    // code and theory BFS- https://www.youtube.com/watch?v=o2LEC2NEVuk&ab_channel=NikhilLohia 
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null){
